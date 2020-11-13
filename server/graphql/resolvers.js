35667408404becb73b85c5c2c9f0e81730ffe4eb
Tom <tomaszsobkowicz21@gmail.com>
2020-11-13 18:21:14 +0100

    Server and database  setup

---
+++ server/graphql/resolvers.js
@@ -0,0 +1,15 @@
+/* eslint-disable no-console */
+const { User } = require('../models');
+
+module.exports = {
+  Query: {
+    getUsers: async () => {
+      try {
+        const users = await User.findAll();
+        return users;
+      } catch (err) {
+        console.log(err);
+      }
+    },
+  },
+};
---
+++ server/server.js
@@ -0,0 +1,19 @@
+/* eslint-disable no-console */
+const { ApolloServer } = require('apollo-server');
+const typeDefs = require('./graphql/typeDefs');
+const resolvers = require('./graphql/resolvers');
+const { sequelize } = require('./models');
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
+
+server.listen().then(({ url }) => {
+  console.log(`🚀 Server ready at ${url}`);
+
+  sequelize
+    .authenticate()
+    .then(() => console.log('Database connected!!'))
+    .catch((err) => console.log(err));
+});

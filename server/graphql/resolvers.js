6baffe47f8abf2bb9d265fd676553c53f26ce48f
Tom <tomaszsobkowicz21@gmail.com>
2020-11-16 13:55:27 +0100

    Modify getAllUsers to exclude user that is logged in

--- server/graphql/resolvers.js
+++ server/graphql/resolvers.js
@@ -2,16 +2,30 @@
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const { UserInputError, AuthenticationError } = require('apollo-server');
+const { Op } = require('sequelize');
 const { User } = require('../models');
 
 module.exports = {
   Query: {
-    getUsers: async () => {
+    getUsers: async (_, __, context) => {
       try {
-        const users = await User.findAll();
+        let user;
+        if (context.req && context.req.headers.authorization) {
+          const token = context.req.headers.authorization.split('Bearer ')[1];
+          jwt.verify(token, process.env.JWT_SECRET, (err, decodedToken) => {
+            if (err) {
+              throw new AuthenticationError('Unauthenticated');
+            }
+            user = decodedToken;
+          });
+        }
+        const users = await User.findAll({
+          where: { username: { [Op.ne]: user.username } },
+        });
         return users;
       } catch (err) {
         console.log(err);
+        throw err;
       }
     },
     login: async (_, { username, password }) => {

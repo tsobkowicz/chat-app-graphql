16f74ca380e787b223b492215968d9cac82801a0
Tom <tomaszsobkowicz21@gmail.com>
2020-11-19 12:55:27 +0100

    Create messages context, refactor Home component

--- client/src/App.js
+++ client/src/App.js
@@ -4,25 +4,28 @@ import { Container } from 'react-bootstrap';
 import ApolloProvider from './ApolloProvider';
 import './App.scss';
 import Register from './pages/Register';
-import Home from './pages/Home';
+import Home from './pages/home/Home';
 import Login from './pages/Login';
 
 import { AuthProvider } from './context/auth';
+import { MessageProvider } from './context/message';
 import DynamicRoute from './util/DynamicRoute';
 
 function App() {
   return (
     <ApolloProvider>
       <AuthProvider>
-        <Router>
-          <Container className="pt-5">
-            <Switch>
-              <DynamicRoute exact path="/" component={Home} authenticated />
-              <DynamicRoute path="/register" component={Register} guest />
-              <DynamicRoute path="/login" component={Login} guest />
-            </Switch>
-          </Container>
-        </Router>
+        <MessageProvider>
+          <Router>
+            <Container className="pt-5">
+              <Switch>
+                <DynamicRoute exact path="/" component={Home} authenticated />
+                <DynamicRoute path="/register" component={Register} guest />
+                <DynamicRoute path="/login" component={Login} guest />
+              </Switch>
+            </Container>
+          </Router>
+        </MessageProvider>
       </AuthProvider>
     </ApolloProvider>
   );
